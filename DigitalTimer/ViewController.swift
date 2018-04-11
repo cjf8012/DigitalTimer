5039f44a5a29a17dee07d52700e5c693cb064856
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-11 12:33:47 +0900

    Switch를 on/off 했을 때 시간이 실행/중지 된다.

--- DigitalTimer/ViewController.swift
+++ DigitalTimer/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var Switch: UISwitch!
     var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-        //클로져(Closure)의 사용 , func에 @objc를 사용 안해도 된다.
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
+//클로져(Closure)의 사용 , func에 @objc를 사용 안해도 된다.
+//        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
+        Switch.setOn(false, animated: true)
     }
 //    @objc func updateTime(){
 //        let date = Date()
@@ -30,6 +32,13 @@ class ViewController: UIViewController {
         formatter.dateFormat = "HH:mm:ss EE YYYY"
         timeLabel.text = formatter.string(from: date)
     }
-
+    @IBAction func TimerStart(_ sender: Any) {
+        if Switch.isOn == true {
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
+        } else {
+            myTimer.invalidate()
+        }
+    }
+    
 }
 
