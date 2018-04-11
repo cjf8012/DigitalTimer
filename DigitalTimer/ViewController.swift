ae9eeec0c49f2bd8c99dcb183a089daf66082f37
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-11 12:17:18 +0900

    클로져(Closure)의 사용

--- DigitalTimer/ViewController.swift
+++ DigitalTimer/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        //클로져(Closure)의 사용 , func에 @objc를 사용 안해도 된다.
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()})
     }
-    @objc func updateTime(){
+//    @objc func updateTime(){
+//        let date = Date()
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "HH:mm:ss EE YYYY"
+//        timeLabel.text = formatter.string(from: date)
+//    }
+    func updateTime(){
         let date = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss EE YYYY"
